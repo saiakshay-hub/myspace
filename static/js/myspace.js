83ef8703e8a24a2fefa780f65ebbf125c80b08d0
saiakshay-hub <psaiakshay21@gmail.com>
2025-07-20 19:52:44 +0530

    Add files via upload

---
+++ static/js/myspace.js
@@ -0,0 +1,6 @@
+ setTimeout(() => {
+            document.getElementById("splash-screen").style.display = "none";
+            document.querySelector("header").style.display = "block";
+        }, 3000); // 3 seconds delay
+
+
